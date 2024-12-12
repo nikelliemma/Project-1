cde2488a127f714a2423d356d29300b4040a5919
nikelliemma <sdi2000152@di.uoa.gr>
2024-12-12 21:30:56 +0200

    Basic command line parsing

--- RunTest.cpp
+++ RunTest.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "dataset.h"
 #include "Graph.h"
 
@@ -55,7 +59,148 @@ std::vector<std::vector<int>> readClosestPoints(const std::string& filePath) {
 }
 
 
+int main(int argc, char *argv[]){
 
+    char *filename = NULL;
+    int option, k = -1, a = -1, L = -1, R = -1, o = -1, S = -1;
+    extern char *optarg;
+    extern int optopt, optind;
+
+    //GETTING ARGUMENTS FROM COMMAND LINE//
+
+    while((option = getopt(argc, argv, ":f:k:a:l:r:o:s:")) != -1){
+        switch (option)
+        {
+        case 'f':
+            if(filename == NULL){
+                filename = optarg;
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+        case 'k':
+            if(k == -1){
+                if((k = atoi(optarg)) <= 0 ){
+                    printf("Malformed input\n");
+                    exit(1);
+                }
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+            break;
+        case 'a':
+            if(a == -1){
+                if((a = atoi(optarg)) <= 0 ){
+                    printf("Malformed input\n");
+                    exit(1);
+                }
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+        case 'l':
+            if(L == -1){
+                if((L = atoi(optarg)) <= 0 ){
+                    printf("Malformed input\n");
+                    exit(1);
+                }
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+            break;
+        case 'r':
+            if(R == -1){
+                if((R = atoi(optarg)) <= 0 ){
+                    printf("Malformed input\n");
+                    exit(1);
+                }
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+            break;
+        case 'o':
+            if(o == -1){
+                o = atoi(optarg);
+                if(o < 1 && o > 3){
+                    printf("Malformed input\n");
+                    exit(1);
+                }
+                break;
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+            break;
+        case 's':
+            if(S == -1){
+                if(o == 3){
+                    if((S = atoi(optarg)) <= 0 ){
+                        printf("Malformed input\n");
+                        exit(1);
+                    }
+                    break;
+                    
+                }else{
+                    cout << "Option -s can be used only when option -o is 3" << endl;
+                    exit(1);
+                }
+            }else{
+                printf("Argument duplicates. Terminating\n");
+                exit(1);
+            }
+            break;
+        case ':':   
+            printf("Missing argument. Terminating\n");
+            exit(1);
+        case '?':
+            printf("Unkown argument. Terminating\n");
+            exit(1);
+        default:
+            printf("Unknow error\n");
+            exit(1);
+        }
+    }
+
+    //check for excess arguments
+    for(; optind < argc ; optind++){
+        printf("Too many arguments. Terminating\n");
+        exit(1);
+    }
+
+
+    
+    switch(o)
+    {
+        case 1:
+            //run vamana
+            break;
+        case 2:
+            //run filtered
+            break;
+        case 3:
+            if(S == -1){
+                printf("Missing argument. Terminating\n");
+                exit(1);
+            }
+            //run stitched
+            break;
+        default:
+            printf("Unknow error. Terminating\n");
+            exit(1);
+    }
+}
+
+
+
+/*
 int main(int argc, char*argv[]){
 
     if(argc < 5){
@@ -96,7 +241,7 @@ int main(int argc, char*argv[]){
 
     std::vector<std::vector<float>> unfiltered;
     RRGraph Vam(R);
-    /*
+    
     if(flag){   //if there are unfiltered
         size = d.get_dataset().size();
         for(int i = 0; i < size; i++){
@@ -107,7 +252,7 @@ int main(int argc, char*argv[]){
         cout << "out of vamana" << endl;
         Vam.print_graph();
     }
-    */
+    
     if(flag < q.get_dataset().size()){ //if there are filtered
         cout << "about to do stitched" << endl;
         GraphCollection collectionOfGraphs = v.StitchedVamana(d, L, R, Rst, alpha);
@@ -130,5 +275,4 @@ int main(int argc, char*argv[]){
 
     return 0;
 }
-
-
+*/
